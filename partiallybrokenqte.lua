9a3ba2a4e8f37055863117b7d00e78da99881c75
grobshlob <140046673+grobshlob@users.noreply.github.com>
2026-04-14 13:00:30 -0500

    Update partiallybrokenqte.lua

--- partiallybrokenqte.lua
+++ partiallybrokenqte.lua
@@ -1,6 +1,11 @@
 send_notification("dont move once u execute", "warning")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sploiter13/severefuncs/refs/heads/main/merge2.lua"))()
-
+print("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+print("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+print("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+print("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+print("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+print("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
 local player = game:GetService("Players")
 local ws = game:GetService("Workspace")
 local lp = player.LocalPlayer
@@ -139,5 +144,6 @@ task.spawn(function()
         if getthebob() then watersplash() end
     end
 end)
+
 
 send_notification("fishing bot running", "info")
