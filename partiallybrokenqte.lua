637d0ad5c78ad1ea1796224d583e385ce5c78b72
grobshlob <140046673+grobshlob@users.noreply.github.com>
2026-04-15 18:10:10 -0500

    Update partiallybrokenqte.lua

--- partiallybrokenqte.lua
+++ partiallybrokenqte.lua
@@ -1,5 +1,5 @@
-send_notification("dont move once u execute: 29", "warning")
-print("HI i updated29")
+send_notification("dont move once u execute: 30", "warning")
+print("HI i updated30")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sploiter13/severefuncs/refs/heads/main/merge2.lua"))()
 
 local player = game:GetService("Players")
@@ -19,12 +19,14 @@ local function getDistance(a, b)
 end
 
 local function spookedfishcheck()
+	print("chec")
 	local gui = game.Players.LocalPlayer.PlayerGui.NotifierGui.MessageContainer
 	local ok, noti = pcall(function()
 		gui:FindFirstChild("Notification")
 	end)
 	if ok and noti then
 		local text = noti.Text
+		print("FOUND")
 		if text == "The fish here are spooked. Move to a new spot!" then
 			return true
 		else return false
@@ -33,26 +35,18 @@ local function spookedfishcheck()
 end
 
 local function resetfish()
-	keypress(0x20)
-	task.wait(0.1)
-	keypress(0x53)
-	task.wait(3)
-	keyrelease(0x53)
-	task.wait(0.1)
-	keypress(0x57)
-	task.wait(3)
-	keyrelease(0x57)
-	task.wait(0.1)
-	keypress(0x41)
-	task.wait(3)
-	keyrelease(0x41)
-	task.wait(0.1)
-	keypress(0x44)
-	task.wait(3)
-	keyrelease(0x44)
-	task.wait(0.1)
-	keyrelease(0x20)
+	local rpos = root.Position
+	keypress(0x51)
+	task.wait(0.25)
+	root.CFrame = CFrame.new(rpos.X + 100, rpos.y + 10, rpos.Z + 100)
+	task.wait(5)
+	keyrelease(0x51)
+	task.wait(0.5)
+	keypress(0x51)
+	task.wait(0.25)
 	root.CFrame = CFrame.new(ogpos)
+	task.wait(0.5)
+	keyrelease(0x51)
 	task.wait(1)
 	mouse1click()
 end
@@ -94,7 +88,6 @@ task.spawn(function()
                     return qteLabel.Text
                 end)
                 if ok2 and text and text ~= "" then
-                    getthebob()
                     local clean = text:gsub("%s", ""):upper()
                     local keycode = string.byte(clean)
                     keypress(keycode)
@@ -103,19 +96,32 @@ task.spawn(function()
                     print("QTE pressed:", clean)
 					if not reset then
 						reset = true
-						local dist2 = getDistance(root.Position, ogpos)
-						if dist2 > 2 then
+						repeat 
+    						task.wait(0.1) 
+    						local _, currentVis = pcall(function() return container.Visible end)
+						until currentVis == false
+						local char = lp.Character
+						if not char or char == nil then return end
+						local croot = char and char:FindFirstChild("HumanoidRootPart")
+						if croot then
+							print("HI67")
 							task.wait(3)
-							root.CFrame = CFrame.new(ogpos)
-							ogpos = nil
-							task.wait(1)
-							mouse1click()
-							task.wait(0.5)
-							keyrelease(0x45)
-							keypress(0x45)
-						else
-							print("recieved an item")
-							mouse1click()
+							local rpos = croot.Position
+							local dist2 = getDistance(rpos, ogpos)
+							if dist2 > 2 then
+								task.wait(3)
+								print("GOING")
+								croot.CFrame = CFrame.new(ogpos)
+								task.wait(1)
+								print("ARRIVED")
+								mouse1click()
+								task.wait(0.5)
+								keyrelease(0x45)
+								keypress(0x45)
+							else
+								print("u recieved an item")
+								mouse1click()
+							end
 						end
 						reset = false
                     end
@@ -133,7 +139,7 @@ local function watersplash()
             	local ok, wpos = pcall(function()
 					return b.Position
 				end)
-				if not wpos or wpos == nil then mouse1click() end
+				if not wpos or wpos == nil then print("CLICKING") mouse1click() end
                 if ok and wpos then
 					local dist = getDistance(wpos, bpos)
 					if dist <= radius2 then
@@ -146,9 +152,6 @@ local function watersplash()
 	                    mouse1press()
 						task.wait(0.05)
 						mouse1release()
-						if spookedfishcheck() then
-							resetfish()
-						end
                 	end
 				end
             end
@@ -159,6 +162,7 @@ task.spawn(function()
     while true do
         task.wait(0.2)
         if getthebob() then watersplash() end
+		if spookedfishcheck() then resetfish() end
     end
 end)
 send_notification("fishing bot running,", "info")
