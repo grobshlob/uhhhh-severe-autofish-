4f1930b48eaadeda2993a08d6b0840faa68ea417
grobshlob <140046673+grobshlob@users.noreply.github.com>
2026-04-14 22:01:41 -0500

    Update partiallybrokenqte.lua

--- partiallybrokenqte.lua
+++ partiallybrokenqte.lua
@@ -1,5 +1,5 @@
 send_notification("dont move once u execute", "warning")
-print("HI i updated16")
+print("HI i updated25?")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sploiter13/severefuncs/refs/heads/main/merge2.lua"))()
 
 local player = game:GetService("Players")
@@ -29,6 +29,7 @@ local function getthebob()
             local ok, cpos = pcall(function()
 				return v.Position
 			end)
+			if not ok and not cpos then mouse1click() end
 			if ok and cpos then
 				bpos = cpos
             	local distance = getDistance(rpos, bpos)
@@ -40,6 +41,8 @@ local function getthebob()
 	end
 	return false
 end
+
+local reset = false
 
 task.spawn(function()
     while true do
@@ -60,24 +63,23 @@ task.spawn(function()
                     task.wait(0.05)
                     keyrelease(keycode)
                     print("QTE pressed:", clean)
-                    if ogpos ~= nil then
-						task.wait(2)
-						local distance2 = getDistance(root.Position, ogpos)
-						if distance2 > 2 then
-	                    	print("HI3")
-	                    	root.CFrame = CFrame.new(ogpos)
-	                    	task.wait(1)
-	                    	ogpos = nil
-	                    	mouse1press()
-	                    	task.wait(0.2)
-	                    	mouse1release()
-	                    	keypress(0x45)
-	                    	task.wait(2.5)
-	                    	keyrelease(0x45)
+					if not reset then
+						reset = true
+						local dist2 = getDistance(root.Position, ogpos)
+						if dist2 > 2 then
+							task.wait(3)
+							root.CFrame = CFrame.new(ogpos)
+							ogpos = nil
+							task.wait(1)
+							mouse1click()
+							task.wait(0.5)
+							keyrelease(0x45)
+							keypress(0x45)
+						else
+							print("recieved an item")
+							mouse1click()
 						end
-                    else 
-                    	task.wait(1)
-                    	mouse1click()
+						reset = false
                     end
                 end
             end
@@ -93,7 +95,7 @@ local function watersplash()
             	local ok, wpos = pcall(function()
 					return b.Position
 				end)
-				if not wpos or wpos == nil then return end
+				if not wpos or wpos == nil then mouse1click() end
                 if ok and wpos then
 					local dist = getDistance(wpos, bpos)
 					if dist <= radius2 then
@@ -107,16 +109,39 @@ local function watersplash()
 						task.wait(0.05)
 						mouse1release()
                 	end
-				else
-					print("HI9")
-					task.wait(1)
-					mouse1press()
-					task.wait(0.3)
-					mouse1release()
 				end
             end
         end
     end
+end
+local function spookedfishcheck()
+	local gui = game.Players.lp.PlayerGui.NotifierGui.MessageContainer
+	local ok, noti = pcall(function()
+		gui:FindFirstChild("Notification")
+	end)
+	if ok and noti then
+		local text = noti.Text
+		if text == "The fish here are spooked. Move to a new spot!" then
+			keypress(0x20)
+			task.wait(0.1)
+			keypress(0x53)
+			task.wait(3)
+			keyrelease(0x53)
+			task.wait(0.1)
+			keypress(0x57)
+			task.wait(3)
+			keyrelease(0x57)
+			task.wait(0.1)
+			keypress(0x41)
+			task.wait(3)
+			keyrelease(0x41)
+			task.wait(0.1)
+			keypress(0x44)
+			task.wait(3)
+			keyrelease(0x44)
+			root.CFrame = CFrame.new(ogpos)
+		end
+	end
 end
 task.spawn(function()
     while true do
@@ -124,4 +149,8 @@ task.spawn(function()
         if getthebob() then watersplash() end
     end
 end)
-send_notification("fishing bot running, hold out ur fishing rod and once u cast it will start", "info")
+send_notification("fishing bot running,", "info")
+task.wait(1)
+local rpos = root.Position
+root.CFrame = CFrame.new(ogpos)
+send_notification("hold out ur fishing rod and once u cast it will start. if u moved from where u tped it will not work", "info")
