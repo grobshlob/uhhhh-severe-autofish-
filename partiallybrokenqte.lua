0cb9a27e1b9b4531c9e5495ec49953f44df75966
grobshlob <140046673+grobshlob@users.noreply.github.com>
2026-04-15 18:16:31 -0500

    Update partiallybrokenqte.lua

--- partiallybrokenqte.lua
+++ partiallybrokenqte.lua
@@ -1,5 +1,5 @@
-send_notification("dont move once u execute: 31", "warning")
-print("HI i updated31")
+send_notification("dont move once u execute: 32", "warning")
+print("HI i updated32")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sploiter13/severefuncs/refs/heads/main/merge2.lua"))()
 
 local player = game:GetService("Players")
