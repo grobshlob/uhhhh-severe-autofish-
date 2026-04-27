95901a515514f12a5cdd93b5ae36282f3c51224c
grob <140046673+grobshlob@users.noreply.github.com>
2026-04-26 21:40:48 -0500

    Update autocorpsepart.lua

--- autocorpsepart.lua
+++ autocorpsepart.lua
@@ -37,33 +37,59 @@ local function getDistance(a, b)
     return math.sqrt((a.X - b.X)^2 + (a.Y - b.Y)^2 + (a.Z - b.Z)^2)
 end
 local parts = {"SaintsRightArm", "SaintsRightLeg", "SaintsRibcage", "SaintsLeftArm", "SaintsLeftLeg", "SaintsHeart"}
-
-local function reliabletp(target)
-	local root = char:FindFirstChild("HumanoidRootPart")
-	task.wait(1)
-	keypress(0x51)
-	task.wait(0.2)
-	root.CFrame = CFrame.new(target + Vector3.new(0, 3, 0))
-	task.wait(0.1)
-		root.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
-			task.wait(0.1)
-	task.wait(3.5)
-	keyrelease(0x51)
-	if getDistance(root.Position, target) > 15 then
-		root.Anchored = false
-		keypress(0x51)
-		task.wait(0.2)
-		root.CFrame = CFrame.new(target + Vector3.new(0, 3, 0))
-		task.wait(1)
-		root.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
-		task.wait(0.1)
-		keyrelease(0x51)
-	else
+local guns = {"Taurus .357", "Schofield 6", "Dual Derringers", "Mauser", "Whitney Dragoon", "Colt Ocelot", "Winchester Repeater", "Maverick 88", "DB Shotgun", "Mares Leg"}
+local function checkammo()
+	local ammo2 = false
+	local stored = true
+	local backpack = lp:FindFirstChild("Backpack")
+	for _, u in pairs(backpack:GetChildren()) do
+		if table.find(guns, u.Name) then
+			local storedammo = u:FindFirstChild("StoredAmmo")
+			local ammoinclip = u:FindFirstChild("AmmoInClip")
+			if storedammo.Value >= 8 then
+				ammo2 = true
+			end
+			if ammoinclip.Value <= 2 then
+				stored = false
+				break
+			end
+		end
 	end
-	keyrelease(0x51)
+	return ammo2, stored
+end
+local function newtp(target)
+	local _, stored = checkammo()
+	keypress(0x31)
+	task.wait(0.5)
+	keyrelease(0x31)
+	task.wait(0.2)
+	if not stored then
+		keypress(0x52)
+		task.wait(5)
+		keyrelease(0x52)
+	end
+	task.wait(0.2)
+	keyrelease(0x20)
+	task.wait(0.2)
+	keypress(0x20)
+	task.wait(0.75)
+	local rpos = root.Position
+	mouse1click()
+	task.wait(0.2)
+	for i = 1, 5 do
+		root.CFrame = CFrame.new(target)
+		task.wait(0.1)
+		if getDistance(target, root.Position) < 10 then 
+			break
+		end
+	end
+	keyrelease(0x20)
+	task.wait(0.3)
+	keypress(0x31)
+	task.wait(0.3)
+	keyrelease(0x31)
 end
 
-local time = 300
 local lastkey = ""
 local running = true
 task.spawn(function()
@@ -76,38 +102,12 @@ task.spawn(function()
 		local pressed = getpressedkeys()
 		local current = ""
 		for _, k in pairs(pressed) do
-			if k == "F1" then
-				current = "F1"
-				break
-			elseif k == "F2" then
+			if k == "F2" then
 				current = "F2"
 				break
 			end		
 		end	
-		if current == "F1" and lastkey ~= "F1" then
-			toggle = not toggle
-			if toggle then
-				send_notification("auto fish started, cast to begin", "info")
-				task.spawn(function()
-					while toggle do
-						task.wait(0.2)
-						if getthebob() then watersplash() end
-					end
-				end)
-				task.spawn(function()
-					while true do
-						task.wait(1)
-						time = time - 1
-						if time == 0 then
-							time = 300
-							resetfish()
-						end
-					end	
-				end)
-			else
-				send_notification("auto fish stopped", "info")
-			end
-		elseif current == "F2" and lastkey ~= "F2" then
+		if current == "F2" and lastkey ~= "F2" then
 			toggle2 = not toggle2
 			running = true
 			if toggle2 then
